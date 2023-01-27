b9356ccefe860d2598106324987e272dddae2f2a
Ivan Riazanov <Omega19961@gmail.com>
2023-01-28 01:18:32 +0600

    new data structure

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,14 +1,44 @@
 <template>
     <div class="board">
-        <CardList></CardList>
-        <CardList></CardList>
-        <CardList></CardList>
-
+        <CardList @addNewList="addNewList" v-for="board in boardData" :list="board.cardList" :listName="board.listName">
+        </CardList>
     </div>
 </template>
 
 <script setup lang="ts">
 import CardList from './CardList.vue';
+import { ref, onMounted } from 'vue'
+import type { CardType } from './Card.vue'
+
+interface ColumnType {
+    cardList: CardType[],
+    listName: string
+}
+
+function addNewList(event: any) {
+    console.log(event)
+}
+
+const boardData = ref<ColumnType[]>()
+onMounted(() => {
+    boardData.value = [
+        {
+            cardList: [{ name: 'Проверить тестовое' }],
+            listName: "В очереди"
+        },
+        {
+            cardList: [{ name: 'Написать тестовое' }],
+            listName: "В работе"
+        },
+        {
+            cardList: [{ name: 'Выслать тестовое' }],
+            listName: "Сделано"
+        },
+
+    ]
+})
+
+
 </script>
 
 <style scoped lang="scss">
